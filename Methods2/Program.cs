3fb6e94e698be6b14b6531bfe3d00ea333598af6
Katherine Winters <goble.katherine@gmail.com>
2021-12-06 20:30:19 -0600

    git initalize

---
+++ Methods2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Methods2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(2, 4));
+            Console.WriteLine(Multiply(10, 2));
+          
+        }
+
+            public static int Add(int x, int y)
+            {
+                return x + y;
+            }
+
+            public static int Subtract(int x, int y)
+            {
+                return x - y;
+            }
+            public static int Multiply(int x, int y)
+            { 
+                return x * y;
+            }
+            public static int Divide(int x, int y)
+            {
+                return x / y;
+            }
+
+        }
+    }
+
